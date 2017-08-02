5e64258f65e358ae61f3df3dd6c59bc6fcc42f36
CarlosJSL <carlosjsl95@gmail.com>
2017-08-01 21:29:04 -0300

    Improving the integration test

--- build/tests/integration/integration.test.js
+++ build/tests/integration/integration.test.js
@@ -1,62 +1,69 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var helpers_1 = require("./config/helpers");
+var HTTPStatus = require("http-status");
 describe('Integration tests ', function () {
     describe('GET /api/users/all', function () {
         it('Should return a Json with all users', function (done) {
             helpers_1.request(helpers_1.app)
-                .get('api/users/all')
+                .get('/api/users/all')
                 .end(function (error, res) {
-                helpers_1.expect(res.status).to.equal(200);
+                console.log("aqui:" + res);
+                helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                done(error);
             });
         });
     });
     describe('GET /api/users/:id', function () {
         it('Should return a Json with one user', function (done) {
             helpers_1.request(helpers_1.app)
-                .get("api/users/" + 1)
+                .get("/api/users/" + 1)
                 .end(function (error, res) {
-                helpers_1.expect(res.status).to.equal(200);
+                helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                done(error);
             });
         });
     });
-    describe('POST /api/users/new', function () {
+    describe('POST /api/users/create', function () {
         it('Should create a new user', function (done) {
             var user = {
                 nome: 'Teste'
             };
             helpers_1.request(helpers_1.app)
-                .post("api/users/new")
+                .post("/api/users/create")
                 .send(user)
                 .end(function (error, res) {
-                helpers_1.expect(res.status).to.equal(200);
+                helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                done(error);
             });
         });
     });
-    describe('PUT /api/users/:id/edit', function () {
-        it('Should update a user', function () {
+    describe('PUT /api/users/:id/update', function () {
+        it('Should update a user', function (done) {
             var user = {
                 id: 1,
                 nome: 'TesteUpdate'
             };
             helpers_1.request(helpers_1.app)
-                .post("api/users/" + 1 + "/edit")
+                .put("/api/users/" + 1 + "/update")
                 .send(user)
                 .end(function (error, res) {
-                helpers_1.expect(res.status).to.equal(200);
+                helpers_1.expect(res.status).to.equal(HTTPStatus.OK);
+                done(error);
             });
         });
     });
-    describe('DELETE /api/users/:id', function () {
+    describe('DELETE /api/users/:id/destroy', function () {
         it('Should delete a user', function (done) {
             var user = {
                 nome: 'TesteUpdate'
             };
             helpers_1.request(helpers_1.app)
-                .delete("api/users/" + 1)
+                .delete("/api/users/" + 1 + "/destroy")
                 .send(user)
                 .end(function (error, res) {
-                helpers_1.expect(res.status).to.equal(204);
+                helpers_1.expect(res.status).to.equal(HTTPStatus.NO_CONTENT);
+                done(error);
             });
         });
     });
--- tests/integration/integration.test.ts
+++ tests/integration/integration.test.ts
@@ -1,68 +1,111 @@
 import {app, request,expect} from './config/helpers'
-
+import * as  HTTPStatus from 'http-status';
 
 describe('Integration tests ', () =>{
     
+    'use strict';
+    const config = require('../../server/config/env/config')()
+    const model = require('../../server/models')
+
+    let id;
+
+    const userTest = {
+        id: 100,
+        name: 'Usuario Teste',
+        email: 'teste@email.com',
+        password: 'teste'
+    }
+
+    const userDefault = {
+        id: 1,
+        name: 'Default User',
+        email:'default@gmail.com' ,
+        password:'default'
+    }
+
+    beforeEach((done)=>{
+        // excluir todos os registros que estao na base de dados, garantindo que um teste nao interfira no resultado do outro
+        model.User.destroy({
+            where:{}
+        }) 
+        .then(()=>{
+            return model.User.create(userDefault)
+            .then(user => {
+                model.User.create(userTest)
+                .then(() => {
+                    done();
+                })
+            })
+        })
+    })
+
     describe('GET /api/users/all',() => {
         it('Should return a Json with all users', done =>{
             request(app)
-                .get('api/users/all')
+                .get('/api/users/all')
                 .end( (error,res)=> {
-                    expect(res.status).to.equal(200);
+                    console.log("aqui:"+res)
+                    expect(res.status).to.equal(HTTPStatus.OK);
+                    done(error)
                 })
         })
     })
+
 
     describe('GET /api/users/:id',() => {
         it('Should return a Json with one user',done =>{
             request(app)
-                .get(`api/users/${1}`)
+                .get(`/api/users/${1}`)
                 .end( (error,res)=> {
-                    expect(res.status).to.equal(200);
+                    expect(res.status).to.equal(HTTPStatus.OK);
+                    done(error)
                 })
         })
     })
 
-    describe('POST /api/users/new',() => {
+    describe('POST /api/users/create',() => {
         it('Should create a new user',done =>{
 
             const user = {
                 nome: 'Teste'
             }
              request(app)
-                .post(`api/users/new`)
+                .post(`/api/users/create`)
                 .send(user)
                 .end( (error,res)=> {
-                    expect(res.status).to.equal(200);
+                    expect(res.status).to.equal(HTTPStatus.OK);
+                    done(error)
                 })
         })
     })
 
-    describe('PUT /api/users/:id/edit',() => {
-        it('Should update a user',() =>{
+    describe('PUT /api/users/:id/update',() => {
+        it('Should update a user',done =>{
             const user = {
                 id: 1,
                 nome: 'TesteUpdate'
             }
              request(app)
-                .post(`api/users/${1}/edit`)
+                .put(`/api/users/${1}/update`)
                 .send(user)
                 .end( (error,res)=> {
-                    expect(res.status).to.equal(200);
+                    expect(res.status).to.equal(HTTPStatus.OK);
+                    done(error)
                 })
         })
     })
 
-    describe('DELETE /api/users/:id',() => {
+    describe('DELETE /api/users/:id/destroy',() => {
         it('Should delete a user',done =>{
             const user = {
                 nome: 'TesteUpdate'
             }
              request(app)
-                .delete(`api/users/${1}`)
+                .delete(`/api/users/${1}/destroy`)
                 .send(user)
                 .end( (error,res)=> {
-                    expect(res.status).to.equal(204);
+                    expect(res.status).to.equal(HTTPStatus.NO_CONTENT);
+                    done(error)
                 })
         })
     })
